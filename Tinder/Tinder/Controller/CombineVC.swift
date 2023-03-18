ed1ce26f687188fdff18771ff10ed6d495e2acb7
Amanda Silva <amanda.silva@xpi.com.br>
2023-03-18 20:58:44 -0300

    configuração inicial

---
+++ Tinder/Tinder/Controller/CombineVC.swift
@@ -0,0 +1,16 @@
+//
+//  CombineVC.swift
+//  Tinder
+//
+//  Created by Amanda Silva on 18/03/23.
+//
+
+import UIKit
+
+class CombineVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.systemGroupedBackground
+    }
+}
